1fd35ec239b423a53f1b9b7ddb7a5cfbfa18c2e1
Angela Zhang <a_zhang93@hotmail.com>
2022-06-27 19:36:32 +1000

    initialapi data fetch

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const SearchExercises = () => {
+
+  const URL = "https://exercisedb.p.rapidapi.com/exercises";
+
+  const[search, setSearch] = useState('');
+
+  const handleSearch = async () => {
+    if(search) {
+      const exerciseData = await fetchData("https://exercisedb.p.rapidapi.com/exercises", exerciseOptions);
+      console.log(exerciseData);
+    }
+  }
+
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{
+        fontSize: { lg: '44px', xs: '30px'}}} 
+        mb="50px" textAlign="center">
+        Awesome Exercise You <br /> 
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField sx={{
+          input: {
+            fontWeight: '700', 
+            border: 'none', 
+            borderRadius:'4px',
+          },
+          width: {lg: '800px', xs:'350px'},
+          backgroundColor: '#fff',
+          borderRadius:'40px'
+
+        }} height="76px" value={search} onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())} placeholder="Search Exercises" type="text"/>
+        <Button className="search-btn" 
+        sx={{
+          width: { lg: '175px', xs: '80px'},
+          fontSize: { lg: '20px', xs: '14px'},
+        }}
+        onClick={handleSearch}
+        >
+          Search  
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
