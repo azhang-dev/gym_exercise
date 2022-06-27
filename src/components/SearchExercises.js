e95cc07b144f995fb3d25ae37770a77da44d6f6b
Angela Zhang <a_zhang93@hotmail.com>
2022-06-27 20:25:55 +1000

    fetch body parts

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -8,12 +8,34 @@ const SearchExercises = () => {
   const URL = "https://exercisedb.p.rapidapi.com/exercises";
 
   const[search, setSearch] = useState('');
+  const[exercises, setExercises] = useState([]);
+  const[bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExerciseData();
+  }, [])
 
   const handleSearch = async () => {
     if(search) {
       const exerciseData = await fetchData("https://exercisedb.p.rapidapi.com/exercises", exerciseOptions);
       console.log(exerciseData);
+
+      const searchedExercises = exerciseData.filter( (exercise) => exercise.name.toLocaleLowerCase().inlcudes(search)
+      || exercise.target.toLocaleLowerCase().inlcudes(search)
+      || exercise.equipment.toLocaleLowerCase().inlcudes(search)
+      || exercise.bodyPart.toLocaleLowerCase().inlcudes(search)
+      );
+
+      setSearch ('')  ;
+      setExercises(searchedExercises);                               
     }
+
   }
 
   return (
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'c76c3e4928msha9ed1b51b2e2100p1e398fjsn2ddb024724ba',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
